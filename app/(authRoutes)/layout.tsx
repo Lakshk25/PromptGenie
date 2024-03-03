b35408292479e1dc84ea0a18e11015668997faeb
Lakshk25 <raghavkumrawat01039@gmail.com>
2024-03-03 12:22:59 +0530

    clerk provider

---
+++ app/(authRoutes)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react';
+
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+    return (
+        <div className='h-full w-full flex justify-center items-center'>
+            {children}
+        </div>
+    );
+}
